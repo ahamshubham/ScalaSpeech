0ef959c717025fd123b657762ac114e869155730
ahamshubham <ahamshubham@gmail.com>
2017-03-27 12:30:44 +0530

    #1 POC for threads initiated at the correct time

--- src/main/scala/RealTimeAudioPlotterClass.scala
+++ src/main/scala/RealTimeAudioPlotterClass.scala
@@ -4,6 +4,7 @@
 package main.scala
 
 import java.awt.{Color, Dimension, Graphics2D}
+import java.util.{Timer, TimerTask}
 import java.util.concurrent.LinkedBlockingQueue
 
 import scala.collection.mutable.Queue
@@ -11,13 +12,13 @@ import scala.swing.{MainFrame, Panel, SimpleSwingApplication}
 import scala.util.control.Breaks._
 
 
-class RealTimeAudioPlotterClass (queue: LinkedBlockingQueue[Double]) extends Runnable {
-  private val sharedQueue = queue
-  private var audioBatch = new Queue[Double]()
-
-  val batch_size = 1000
+class RealTimeAudioPlotterClass(queue: LinkedBlockingQueue[Double]) extends Runnable {
+  val batch_size = 960
   val initial_queue_size = 3200
   val shift = batch_size
+  private val sharedQueue = queue
+  private val window_maker = new DrawingClass()
+  private var audioBatch = new Queue[Double]()
 
   def run() {
     for (i <- 1 to initial_queue_size) {
@@ -31,39 +32,42 @@ class RealTimeAudioPlotterClass (queue: LinkedBlockingQueue[Double]) extends Run
 
     windowMaker()
 
-    plotter()
-
-    while (true) {
-      try {
-        for (i <- 1 to shift) {
-          val element = sharedQueue.take()
-
-          if (element != null) {
-            audioBatch.dequeue()
-            audioBatch.enqueue(element)
-          }
-          else
-            break
-        }
-
-        plotter()
-
-      } catch {
-
-        case ex: InterruptedException => println("Interrupted Exception")
-
-      }
-    }
-  }
-
-  private val window_maker = new DrawingClass()
-
-  def plotter(): Unit = {
-    window_maker.paintAgain(audioBatch.toList)
+    val plotter_task = new PlotterTask
+    val plotter_timer = new Timer
+    val time_period = shift / 16
+    plotter_timer.scheduleAtFixedRate(plotter_task, 0, time_period)
   }
 
   def windowMaker(): Unit = {
     window_maker.main(Array())
+  }
+
+  def plotter(audioBatchList: List[Double]): Unit = {
+    window_maker.paintAgain(audioBatchList)
+  }
+
+  class PlotterTask extends TimerTask {
+    override def run() = {
+      //This should be converted to a thread
+      println("System time:" + System.currentTimeMillis())
+      plotter(audioBatch.toList)
+
+      try {
+        for (i <- 1 to shift) {
+          val element = sharedQueue.take()
+          if (element != null) {
+            audioBatch.dequeue()
+            audioBatch.enqueue(element)
+          }
+          else {
+            break
+          }
+        }
+      } catch {
+        case ex: InterruptedException => println("Interrupted Exception")
+      }
+      println("Completed_plotting")
+    }
   }
 
 }
@@ -98,26 +102,30 @@ class DataPanel() extends Panel {
     super.paintComponent(g)
 
     //Set color for plotting
-    g.setColor(new Color(29,176,249))
+    g.setColor(new Color(29, 176, 249))
 
-    val mid_y = (g.getClipBounds.height.toFloat)/2
+
+    val window_height = g.getClipBounds.height.toFloat
+    val window_width = g.getClipBounds.width.toFloat
+    val scale = (window_width.toFloat / (audioInfoList.length))
+    val mid_y = window_height / 2
     var x2 = 1
     var prev_point = 0.0
 
     audioInfoList foreach { point =>
       val y1 = prev_point.toInt + mid_y.toInt
       val y2 = point.toInt + mid_y.toInt
-      val x1 = x2-1
+      val x1 = x2 - 1
 
-      g.drawLine(x1,y1,x2,y2)
+      g.drawLine((x1 * scale).toInt, y1, (x2 * scale).toInt, y2)
 
       x2 = x2 + 1
       prev_point = point
     }
   }
 
-  def plot(audioBatch: List[Double]) {
-    this.audioInfoList = audioBatch
+  def plot(audioBatchList: List[Double]) {
+    this.audioInfoList = audioBatchList
   }
 
 }
